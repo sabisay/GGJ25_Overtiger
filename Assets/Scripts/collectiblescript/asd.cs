c0aa867683d4c0f18b4a31e1c7ca5711117d46c3
Furkan Yusuf Çakmak <cfurkanyusuf@gmail.com>
2025-01-25 05:16:57 +0300

    collectible
    collectible

--- Assets/Scripts/collectiblescript/asd.cs
+++ Assets/Scripts/collectiblescript/asd.cs
@@ -3,10 +3,7 @@ using UnityEngine;
 public class Collectible : MonoBehaviour
 {
     // Ses efekti için (isteðe baðlý)
-    public AudioClip collectSound;
-
-    // Puanlama sistemi için (isteðe baðlý)
-    public int scoreValue = 10;
+   
 
     void Start()
     {
@@ -17,26 +14,26 @@ public class Collectible : MonoBehaviour
     {
         // Özel bir animasyon veya dönüþ efekti eklemek isterseniz, bunu burada yapabilirsiniz.
         // Örneðin: Sürekli yavaþça dönme
-        transform.Rotate(Vector3.forward * 50 * Time.deltaTime);
+  
     }
 
     // Çarpýþma tespiti
     private void OnTriggerEnter2D(Collider2D other)
     {
         // Oyuncu etiketi kontrolü
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("sabun"))
         {
-            // Ýsteðe baðlý: Ses efekti çal
-            if (collectSound != null)
-            {
-                AudioSource.PlayClipAtPoint(collectSound, transform.position);
-            }
+            
 
             // Ýsteðe baðlý: Skor artýrma
             // Skor sistemi varsa buradan güncelleyebilirsiniz.
             // ScoreManager.instance.AddScore(scoreValue);
 
             // Nesneyi yok et
+            Destroy(gameObject);
+        }
+        if (other.CompareTag("su"))
+        {
             Destroy(gameObject);
         }
     }
