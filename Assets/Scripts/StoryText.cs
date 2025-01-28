0eaca77a41ea642a53efe2353037f3472d291806
Sabriye Nur <79248086+sabisay@users.noreply.github.com>
2025-01-28 08:49:40 +0300

    mainSceneUpdated

--- Assets/Scripts/StoryText.cs
+++ Assets/Scripts/StoryText.cs
@@ -1,32 +1,20 @@
 using System.Collections;
 using UnityEngine;
 using TMPro;
-using System.Collections.Generic; // TMPro namespace ekliyoruz
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 
 public class StoryText : MonoBehaviour
 {
     public List<string> Stories;
-    public TextMeshProUGUI textMeshPro; // TextMeshPro'yu buraya ekliyoruz
+    public TextMeshProUGUI textMeshPro;
     private int currentIndex = 0;
-    public GameObject menuCanvas;
-    public GameObject pnael;
 
     void Start()
     {
-        PlayerController.Instance.enabled = false;
-        menuCanvas.SetActive(false);
         if (Stories.Count > 0)
         {
             textMeshPro.text = Stories[currentIndex]; // Ýlk metni ekrana yazdýrýyoruz
-        }
-    }
-
-    void Update()
-    {
-        // Ekranýn herhangi bir yerine týklama algýlama (sol fare tuþu)
-        if (Input.GetMouseButtonDown(0)) // 0: Sol fare tuþu
-        {
-            OnClick();
         }
     }
 
@@ -40,19 +28,13 @@ public class StoryText : MonoBehaviour
         else
         {
             // Son metne gelindiðinde 2 saniye bekleyip kapanmayý baþlat
-            StartCoroutine(CloseAfterDelay(1));
+            StartCoroutine(CloseAfterDelay(2));
         }
     }
 
-    public void CanvasClose()
-    {
-        menuCanvas.SetActive(true);
-    }
-
-    private IEnumerator CloseAfterDelay(float delay)
+    private IEnumerator CloseAfterDelay(int delay)
     {
         yield return new WaitForSeconds(delay);
-        menuCanvas.SetActive(true);
-        Destroy(pnael);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
